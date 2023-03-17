1cdb2ffd9c16999d6be5e62c9f1e9acc8111cfb4
Kevink12341 <kevin.kuiper@gmail.com>
2023-03-17 16:13:38 +0100

    working on sorting

--- src/components/ArrToTable.tsx
+++ src/components/ArrToTable.tsx
@@ -1,86 +1,112 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core'; 
+import { faFontAwesome, faMagnifyingGlass, faSortAsc } from "@fortawesome/free-solid-svg-icons";
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import React, { ReactElement, ReactNode } from "react"
 import { Form, Row } from "react-bootstrap";
 import Table from 'react-bootstrap/Table';
 
+library.add(fas, faFontAwesome)
+
 interface Props {
-    data?: Array<any>;
+    data: Array<any>;
     any?: any;
 };
 
 interface State{
-  data?: Array<any>;
+  data: Array<any>;
   header?: any;
-  searchValue?: Array<any>;
+  searchArray?: Array<any>;
 };
 
 class ArrToTable extends React.Component <Props, State> {
     constructor(props: Props) {
         super(props);
-        this.state = {}
+        this.state = { data: []};
 
         this.fetchData = this.fetchData.bind(this);
         this.searchHandler = this.searchHandler.bind(this);
     }
     
     fetchData(){
-        let data = this.props.data
-        this.setState({data: data})
-    }
+        let data = this.props.data;
+        this.setState({data: data});
+    };
+
     componentDidMount(): void {
-      this.fetchData()
-    }
+      this.fetchData();
+    };
 
     searchHandler(event:any){
-      let {searchValue} = this.state
-      let searchValueUnformatted = event.target.value != undefined ? event.target.value : ""
+      let {searchArray} = this.state;
+      let searchValueUnformatted = event.target.value != undefined ? event.target.value : "";
       
-      let data = this.state.data
+      let data = this.state.data;
       let filteredDataArray: Array<any> = [];
       if(data){
         data.forEach((row, index) => {
           if(row){
-            let keys: Array<any> = Object.keys(row)
-            keys.forEach((key)=> {
+            for(const key in row){
               if(row[key].toString().toLowerCase().includes(searchValueUnformatted.toString().toLowerCase())){
                 if(data)
-                filteredDataArray.push(data[index])
-              }
-            })
-          }
+                filteredDataArray.push(data[index]);
+              };
+            };
+          };
         });
-        let removeDuplicates= [...new Set(filteredDataArray)]
-        this.setState({searchValue: removeDuplicates})
+        let removeDuplicates= [...new Set(filteredDataArray)];
+        this.setState({searchArray: removeDuplicates});
       };
     };
 
-    filterCallback(){
-      
+    sorting(event:any, index:number){
+      console.log(event, index)
+      let data = this.state.data
+      let keys: Array<any> = []
+      if(data){
+        keys = Object.keys(data[0])
+      }
+
+      let sortAsc = () => {
+        data.sort((a,b)=>{
+          // console.log(a,b, "logme")
+          const keyA = a[keys[index]].toString().toLowerCase();
+          const keyB = b[keys[index]].toString().toLowerCase();
+          if(keyA < keyB){
+            return -1;
+          }
+          if(keyA > keyB){
+            return 1;
+          }
+          return 0
+        })
+
+      };
+      let sortDesc = () => {
+
+      };
+    sortAsc()
     }
-
-
 
     render() {
 
-      let {data, searchValue} = this.state
-      if(searchValue){
-        data =  searchValue
-      }
+      let {data, searchArray} = this.state;
+      if(searchArray){
+        data =  searchArray;
+      };
 
       let rows:Array<ReactNode> = [];
       if(data){
-      
         data.forEach((row,index) =>{
-          let columns:Array<ReactElement> = []
-          let keys = Object.keys(row)
+          let columns:Array<ReactElement> = [];
+          let keys = Object.keys(row);
           keys.forEach((item,index) => {
             columns.push(
               <td key={index}>
                 {row[item]}
               </td>
             )
-          })
+          });
           if(columns){
             rows.push(
               <tr key={index}>
@@ -89,22 +115,26 @@ class ArrToTable extends React.Component <Props, State> {
                 })}
               </tr>
               )
-          }
-        })
-      }
+          };
+        });
+      };
      
       return ( 
       <>
         <Form.Group>
-          <Form.Control placeholder="Search" onChange={e => {this.searchHandler(e)}}/>
+          <>
+          <Form.Control placeholder="Search" onChange={e => {this.searchHandler(e)}}>
+          </Form.Control>
           <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </>
         </Form.Group>
 
         <Table striped bordered hover>
           <thead>
             <tr>           
-              {this.state.data && Object.keys(this.state.data[0]).map((header, index) =>(
-                <th key={index} > {header} </th>
+              {this.state.data.length > 0 && Object.keys(this.state.data[0]).map((header, index) =>(
+                <th key={index} > {header} &nbsp; <FontAwesomeIcon className="float-end" icon="sort" onClick={e => {this.sorting(e,index)}} /></th>
+      
               ))}
             </tr>
           </thead>
