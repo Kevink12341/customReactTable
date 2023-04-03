e223bdc8489a1ab218394d55a98090fdcc480f4d
Kevink12341 <kevin.kuiper@gmail.com>
2023-04-03 12:25:52 +0200

    update sorting function

--- src/helpers/sorting.ts
+++ src/helpers/sorting.ts
@@ -1,58 +1,75 @@
-function mySortFunction(data:Array<any>, key:any, direction?:string ): Array<any> {
-
-    if(data[0][key] && direction || data[0][key] === 0){
-        if(typeof(data[0][key]) == "string" && direction =="asc"){
-            data = sortAscString(data, key)
+function mySortFunction(data:Array<any>, searchKey:any, direction?:string ): Array<any> {
+    // sorting for Objects
+    // current methods string | number | mixed sort
+    if(typeof(data[0]) === "object"){
+        let searchKeyType = sortingHelper(data,searchKey)
+        if(searchKeyType === "string" && direction === "asc"){
+            data.sort((a,b)=>{a=a[searchKey], b=b[searchKey]; return sortStringAscending(a, b)})
         };
-        if(typeof(data[0][key]) == "string" && direction =="desc"){
-            data = sortDescString(data,key)
+        if(searchKeyType === "string" && direction === "desc"){
+            data.sort((a,b)=>{a=a[searchKey], b=b[searchKey]; return sortStringDescending(a, b)})
         };
-        if(typeof(data[0][key]) == "number" && direction == "asc"){
-            data = sortAscNumber(data,key)
+        if(searchKeyType === "number" && direction === "asc"){
+            data.sort((a,b) => {a=a[searchKey], b=b[searchKey]; return sortNumberAscending(a,b)})
         };
-        if(typeof(data[0][key]) == "number" && direction == "desc"){
-            data = sortDescNumber(data,key)
+        if(searchKeyType === "number" && direction === "desc"){
+            data.sort((a,b) => {a=a[searchKey], b=b[searchKey]; return sortNumberDescending(a,b)})
+        };
+        if(searchKeyType === "mixed" && direction === "asc"){
+            data.sort((a,b) => {a=a[searchKey]!= undefined ? a[searchKey]: "", b=b[searchKey] != undefined ? b[searchKey]: ""; return sortMixedTypeAscending(a,b)})
+        };   
+        if(searchKeyType === "mixed" && direction === "desc"){
+            data.sort((a,b) => {a=a[searchKey]!= undefined ? a[searchKey]: "", b=b[searchKey] != undefined ? b[searchKey]: ""; return sortMixedTypeDescending(a,b)})
         };
     };
-    return data
+        return data
+    };
+
+function sortingHelper(data: Array<any>, searchKey: "string"):string{
+    let datatype = ""
+    for(let i = 1 ; i< data.length ; i++) {
+        if(typeof(data[i][searchKey]) != typeof(data[i-1][searchKey])){
+            datatype = "mixed"
+            break
+        }
+        else{
+            datatype =  typeof(data[i][searchKey])
+        };
+    };
+
+    return datatype
 };
 
-let sortAscString = (data:Array<any>, key:any) => {
-    data.sort((a, b) => {
-        const keyA:any = a[key] != undefined ? a[key].toString().toLowerCase() : "";
-        const keyB:any = b[key] != undefined ? b[key].toString().toLowerCase() : "";
-        return keyA < keyB ? -1 : 1
-    })
-    return data;
+//string sort
+function sortStringAscending(a:string,b:string){
+    const aString = a.toLowerCase();
+    const bString = b.toLowerCase();
+    return aString < bString ? -1 : 1
+};
+function sortStringDescending(a:string, b:string){
+    const aString = a.toLowerCase();
+    const bString = b.toLowerCase();
+    return aString > bString ? -1 : 1
 };
 
-let sortDescString = (data:Array<any>, key:any) => {
-    data.sort((a, b) => {
-        const keyA:any = a[key] != undefined ? a[key].toString().toLowerCase() : "";
-        const keyB:any = b[key] != undefined ? b[key].toString().toLowerCase() : "";
-        return keyA > keyB ? -1 : 1
-    });
-    return data;
+//number sort
+function sortNumberAscending(a:number, b:number){
+    return a-b
+};
+function sortNumberDescending(a:number, b:number){
+    return b-a
 };
 
-let sortAscNumber = (data:Array<any>, key:any) => {
-    data.sort((a, b) => {
-        const keyA:any = a[key] != undefined ? a[key] : 0;
-        const keyB:any = b[key] != undefined ? b[key] : 0;
-        return keyA < keyB ? -1 : 1
-    })
-    return data;
+//converting mix sort
+function sortMixedTypeAscending(a:any, b:any){
+    const mixedA = a.toLocaleString() != undefined || null ? a.toLocaleString().toLowerCase() : "";
+    const mixedB = b.toLocaleString() != undefined || null ? b.toLocaleString().toLowerCase() : "";
+    return mixedA < mixedB ? -1 : 1
 };
-
-let sortDescNumber = (data:Array<any>, key:any) => {
-    data.sort((a, b) => {
-        const keyA:any = a[key] != undefined ? a[key] : 0;
-        const keyB:any = b[key] != undefined ? b[key] : 0;
-        return keyA > keyB ? -1 : 1
-    })
-    return data;
+function sortMixedTypeDescending(a:any, b:any){
+    const mixedA = a.toString() != undefined ? a.toString().toLowerCase() : "";
+    const mixedB = b.toString() != undefined ? b.toString().toLowerCase() : "";
+    return mixedA > mixedB ? -1 : 1
 };
-
-
 
 export default mySortFunction
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import ArrToTable from './components/ArrToTable';
 import { data } from './redditposts';
 
-let users = [{ id: 1, email: "john@doe.nl", password: "", name: "asdasdasdasdasd" }, { id: 1, email: undefined, password: "12345", name: "asdasdasdasdasd" }, { id: 1, email: "john@doe.nl", password: "asdasdasdasdasd", name: "asdasdasdasdasd" }, { id: 1, email: "john@doe.nl", password:"something", name:"cookies" }, { id: 1, email: "john@doe.nl", password: "asdasdasdasdasd", name: "asdasdasdasdasd" }, { id: 2, email: "doe@john.nl", password: "312453454", name: "xddxxdcd" }, { id: 0, email: "abc@def.nl", password: "aaaaa", name: "abc" }, { id: 1, email: undefined, password: "dcasfdas23", name: "gdfgdf322asdf" }]; // array of users
+let users = [{ id: 1, email: "john@doe.nl", password: "", name: "asdasdasdasdasd" }, { id: 1, email: undefined, password: "12345", name: "asdasdasdasdasd" }, { id: 1, email: "john@doe.nl", password: "asdasdasdasdasd", name: "asdasdasdasdasd" }, { id: 1, email: "john@doe.nl", password:"something", name:"cookies" }, { id: 1, email: "john@doe.nl", password: "asdasdasdasdasd", name: "asdasdasdasdasd" }, { id: 2, email: "doe@john.nl", password: 312453454, name: "xddxxdcd" }, { id: 0, email: "abc@def.nl", password: "aaaaa", name: "abc" }, { id: 1, email: undefined, password: "dcasfdas23", name: "gdfgdf322asdf" }]; // array of users
 
 
 
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ArrToTable striped hover data={data} variant="dark" size="sm" responsive="sm" />
+    <ArrToTable striped hover data={users} variant="dark" size="sm" responsive="sm" />
   </React.StrictMode>
 );
 
