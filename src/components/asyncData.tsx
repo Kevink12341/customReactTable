8e61659ed66de2d8d582fa64f9d20c6f3a55c289
Kevink12341 <kevin.kuiper@gmail.com>
2023-05-10 23:42:29 +0200

    hello Theriel

--- src/components/asyncData.tsx
+++ src/components/asyncData.tsx
@@ -15,7 +15,8 @@ class AsyncData extends React.Component<{},State>{
 
     componentDidMount() {
         let url = "https://odata4.cbs.nl/CBS/83878NED/Observations";
-        fetchUrl(url).then((data) => {(console.log(data),  this.setState({ data: data.value }));})
+        let url2= `https://kronos.elusive-dev.com/api/Leaderboard/500/0`
+        fetchUrl(url2).then((data) => {(console.log(data),  this.setState({ data: data.entries }));})
     };
 
     render(): React.ReactNode {
